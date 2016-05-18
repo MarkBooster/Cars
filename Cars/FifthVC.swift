be2ee1b957ac19c7312965b6969e9ca3068542b9
Mark van Winden <MarkvanWinden@iMac-van-Mark.local>
2016-05-18 06:26:24 +0200

    Finished 3-5 VC.

--- Cars/FifthVC.swift
+++ Cars/FifthVC.swift
@@ -7,10 +7,27 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class FifthVC: UIViewController {
 
+    @IBOutlet weak var videoContainer: UIView!
+    
+    var moviePlayer: MPMoviePlayerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        moviePlayer = MPMoviePlayerController(contentURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("bmwX6", ofType: "mp4")!))
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        videoContainer.addSubview(moviePlayer.view)
+        let frame = CGRectMake(0, 0, videoContainer.bounds.width, videoContainer.bounds.height)
+        moviePlayer.view.frame = frame
+        moviePlayer.shouldAutoplay = false
+        moviePlayer.prepareToPlay()
     }
 }
